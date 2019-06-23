ed0568aa3d41a8fd6c03275a7c7451cbd37769ac
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-24 01:26:04 +0200

    create one to many between project model and users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_225819) do
+ActiveRecord::Schema.define(version: 2019_06_23_230056) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
